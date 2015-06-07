547a9ad3012c88cc56d1e506b657e718d09ab8ac
Fränz Friederes <fraenz@frieder.es>
2015-06-08 00:14:23 +0200

    Add intro logo animation

---
+++ Lidderbuch/LBIntroViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LBIntroViewController.swift
+//  Lidderbuch
+//
+//  Created by Fränz Friederes on 07/06/15.
+//  Copyright (c) 2015 ACEL. All rights reserved.
+//
+
+import UIKit
+
+class LBIntroViewController: UIViewController
+{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var frame: Int = -1
+    private let frameCount: Int = 25
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        imageView.clipsToBounds = true
+        
+        // start animation at 25 fps
+        NSTimer.scheduledTimerWithTimeInterval(0.04, target: self, selector: Selector("animationTick:"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func animationTick(timer: NSTimer)
+    {
+        if ++frame < 25
+        {
+            // move down to next frame
+            let imageLayer = imageView.layer
+            var contentsRect = imageLayer.contentsRect
+            contentsRect.origin.y = CGFloat(frame) * (CGFloat(1.0) / CGFloat(frameCount))
+            imageLayer.contentsRect = contentsRect
+        }
+        else
+        {
+            // animation completed
+            timer.invalidate()
+            
+            // proceed to songbook
+            performSegueWithIdentifier("ShowSongbook", sender: self)
+        }
+    }
+}
---
+++ Lidderbuch/LBReplaceSegue.swift
@@ -0,0 +1,28 @@
+//
+//  LBReplaceSegue.swift
+//  Lidderbuch
+//
+//  Created by Fränz Friederes on 07/06/15.
+//  Copyright (c) 2015 ACEL. All rights reserved.
+//
+
+import UIKit
+
+class LBReplaceSegue: UIStoryboardSegue
+{
+    override func perform() {
+        if let sourceViewController = sourceViewController as? UIViewController,
+            destinationViewController = destinationViewController as? UIViewController,
+            navigationController = sourceViewController.navigationController
+        {
+            let transition = CATransition()
+            transition.duration = 0.2
+            transition.type = kCATransitionFade
+            transition.subtype = kCATransitionFromTop
+            
+            navigationController.view.layer.addAnimation(transition, forKey: kCATransition)
+            navigationController.popToRootViewControllerAnimated(false)
+            navigationController.pushViewController(destinationViewController, animated: false)
+        }
+    }
+}
