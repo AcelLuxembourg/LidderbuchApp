cf727766bef851ae0d74339533bb3ffa5d46cbf8
Fränz Friederes <fraenz@frieder.es>
2016-11-11 23:22:06 +0100

    Removes a comparison of an optional var

--- Lidderbuch/LBLyricsView.swift
+++ Lidderbuch/LBLyricsView.swift
@@ -7,30 +7,6 @@
 //
 
 import UIKit
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func >= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l >= r
-  default:
-    return !(lhs < rhs)
-  }
-}
-
 
 class LBLyricsView: UIScrollView
 {
@@ -350,7 +326,7 @@ class LBLyricsView: UIScrollView
             line = highlightedLine! + 1
         }
         
-        if line >= lineOrigins.count
+        if line! >= lineOrigins.count
         {
             // reached end of lyrics
             line = nil
