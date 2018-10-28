c3f33f799498ca9300c59392420d843ba22aa6cd
Benoît Frisch <benoit@fresh.lu>
2018-10-28 20:11:34 +0100

    Add delay on LaunchScreen

--- Lidderbuch/AppDelegate.swift
+++ Lidderbuch/AppDelegate.swift
@@ -27,6 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
+        Thread.sleep(forTimeInterval: 1.0)
+        
         window?.tintColor = LBVariables.tintColor
         return true
     }
